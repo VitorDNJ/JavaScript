03bda3998eb303a639319e965ac28cf4a139902c
VitorDNJ <vitorjustino980@gmail.com>
2025-11-01 14:26:26 -0300

    Declarando uma variável

---
+++ ex003/ex003.js
@@ -0,0 +1,10 @@
+
+let númeroMáximo = Number .MAX_VALUE;
+console.log("Número Máximo: " , númeroMáximo);
+
+/*
+JavaScript tem um valor máximo de número que pode ser representado, que é acessível
+através da propriedade estática Number.MAX_VALUE. Aqui, estamos atribuindo esse
+valor a uma variável chamada númeroMáximo e, em seguida, imprimindo-a usando
+console.log().
+*/
