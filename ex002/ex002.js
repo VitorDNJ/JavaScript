b7d84a06858719b9943acd545346336678f6f915
VitorDNJ <vitorjustino980@gmail.com>
2025-11-01 14:02:58 -0300

    Declarando uma constante

---
+++ ex002/ex002.js
@@ -0,0 +1,9 @@
+const PI = 3.14159;
+console.log("PI: " , PI);
+
+/*
+Neste exercício, estamos usando const para declarar uma constante. Diferente de uma 
+varíavel declarada com let, uma constante não pode ser reatribuída depois de ser 
+inicializada. Aqui, estamos atribuindo o valor de pi à constante PI e, em seguida, usando 
+console.log() para imprimir o valor na console.
+*/
