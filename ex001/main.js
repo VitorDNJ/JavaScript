c148ebaf9ab545d9cb9819ba3892f6b4fcc3a58d
VitorDNJ <vitorjustino980@gmail.com>
2025-11-02 13:01:02 -0300

    declarando variáveis  e usando operadores lógicos

--- ex001/main.js
+++ ex001/main.js
@@ -3,3 +3,11 @@ let peso = 70;
 
 console.log ("idade: " , idade);
 console.log ("peso: " , peso);
+
+
+/*
+Este exercício envolve a declaração e inicialização de variáveis, e também a impressão
+dos valores dessas variáveis no console. Aqui, let é usado para declarar variáveis idade e
+peso, e então são atribuídos valores a elas. Em seguida, usamos o console.log() para
+imprimir os valores das variáveis.
+*/
