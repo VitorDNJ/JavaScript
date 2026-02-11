fc08ff628a7f844fc9a13564ff15356e90d74d02
VitorDNJ <vitorjustino980@gmail.com>
2026-02-10 22:46:50 -0300

    update

---
+++ livrojs/Capitulo_2_Fundamentos_de_JavaScript/script.js
@@ -0,0 +1,11 @@
+// Variáveis, tipos de dados e operadores
+
+// Exercício 1
+let idade = 25;
+let peso = 70;
+console.log("Idade: ", idade);
+console.log("Peso: ", peso);
+
+// Exercício 2          
+const PI = 3.14159;
+console.log("O valor de PI é ", PI);
