4172cda3be6ede62fe2f01e41d1de4ab7e37df68
VitorDNJ <vitorjustino980@gmail.com>
2026-02-18 22:51:31 -0300

    update

--- 200 exercicios/1_fundamentos/script.js
+++ 200 exercicios/1_fundamentos/script.js
@@ -118,3 +118,15 @@ function calcularIMC(peso, altura){
 calcularIMC(60 , 1.7); // Imprime: Peso normal
 calcularIMC(80 , 1.7); // Imprime: Sobrepeso
 calcularIMC(90 , 1.7); // Imprime: Obesidade
+
+// Exercício 14: 
+function verificarBissexto(ano){
+    if ((ano % 4 == 0 && ano % 100 !=0 ) || (ano % 400 == 0)){
+        console.log(ano + " é um ano bissexto");
+    } else {
+        console.log(ano + " não é um ano bissexto");
+    }
+}
+verificarBissexto(2000); // Imprime: 2000 é um ano bissexto
+verificarBissexto(2001); // Imprime: 2001 não é um ano bissexto
+verificarBissexto(2028); // Imprime: 2028 é um ano bissexto
